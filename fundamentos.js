e6fdf98c908451d12450b2fefd8b97b43337492c
marianicacio <mariaeduarda.nicacio2007@gmail.com>
2025-06-19 21:18:34 -0300

    feat: parada forçada

---
+++ fundamentos.js
@@ -0,0 +1,20 @@
+//Variaveis
+
+//variavel que varia
+let nome = "Maria"
+nome = "Carlos"
+
+console.log(nome)
+
+//variavel constante
+const idade = 20
+// idade = 30
+
+console.log(idade)
+
+//Arrays
+
+const pessoa = {nome : "carlos"}
+pessoa.nome = "Maria"
+
+console.log(pessoa)
