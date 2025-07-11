ee26fd908c402f2c2679db6273ad030936e9a84b
marianicacio <mariaeduarda.nicacio2007@gmail.com>
2025-07-11 18:33:23 -0300

    feat: funcoes pura/impura

--- funcoes.js
+++ funcoes.js
@@ -1,3 +1,14 @@
 export function somar(a,b) {
     return a + b
 }
+
+//Funcão pura 
+function dobro(a) {
+    return a *2
+}
+
+//Funcão inpura
+let total = 0
+function adicionar(x) {
+    total+=x
+}
