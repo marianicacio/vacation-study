38e948d471e8d5f1fecdd492e2c704969cc8a5b2
marianicacio <mariaeduarda.nicacio2007@gmail.com>
2025-07-14 22:12:15 -0300

    feat: preguiça

--- meu-projeto/src/App.jsx
+++ meu-projeto/src/App.jsx
@@ -1,9 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <h1>teste</h1>
+    <body>
+      <header>
+        <h1>teste</h1>
+        <p>Ola mundo</p>
+      </header>
+    </body>
   );
 }
 
