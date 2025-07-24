44165ef6b1a15cb8c9754992d03f7c8a4340196a
marianicacio <mariaeduarda.nicacio2007@gmail.com>
2025-07-24 13:15:51 -0300

    feat: form

--- meu-projeto/src/App.jsx
+++ meu-projeto/src/App.jsx
@@ -46,7 +46,16 @@ function App() {
 
         function handleSubmit(e) {
             e.preventDefault()
+            alert(`Nome: ${nome}\nEmail: ${email}`)
         }
+
+        return (
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder='Nome' value={nome} onChange={(e) => setNome(e.target.value)} />
+                  <input type="text" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
+                  <button type='submit'>Enviar</button>
+            </form>
+        )
     }
 
 
