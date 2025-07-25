3330b9668cbf400223085e4ea7ce81f5c62a63c5
marianicacio <mariaeduarda.nicacio2007@gmail.com>
2025-07-25 12:11:34 -0300

    feat: h1

--- meu-projeto/src/App.jsx
+++ meu-projeto/src/App.jsx
@@ -50,11 +50,14 @@ function App() {
         }
 
         return (
+            <div>
+        <h1>formulario</h1>
             <form onSubmit={handleSubmit}>
                 <input type="text" placeholder='Nome' value={nome} onChange={(e) => setNome(e.target.value)} />
                   <input type="text" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
                   <button type='submit'>Enviar</button>
             </form>
+            </div>
         )
     }
 
